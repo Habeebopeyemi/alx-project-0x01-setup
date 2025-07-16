6b38693a074d2e109c77c88fba8c850e3adca922
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-16 14:27:57 +0100

    feat: task-2 completed

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,8 +1,40 @@
 import React from "react";
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interface";
 
-function index() {
-  return <div>index</div>;
+const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
+  //console.log(posts);
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add Post
+          </button>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2 ">
+          {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+            <PostCard
+              title={title}
+              body={body}
+              userId={userId}
+              key={key}
+              id={id}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+//fetching posts data
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: PostProps[] = await res.json();
+  return { props: { posts } };
 }
-
-export default index;
+export default Posts;
