9e3caf221d8d496faab0e0048883c41c42f920c0
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-16 19:12:45 +0100

    feat: task 3 done

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,61 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-function index() {
-  return <div>index</div>;
+const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add Post
+          </button>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2 ">
+          {users?.map(
+            (
+              {
+                id,
+                name,
+                username,
+                email,
+                address,
+                phone,
+                website,
+                company,
+              }: UserProps,
+              key: number
+            ) => (
+              <UserCard
+                name={name}
+                username={username}
+                email={email}
+                address={address}
+                phone={phone}
+                website={website}
+                company={company}
+                key={key}
+                id={id}
+              />
+            )
+          )}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: UserProps[] = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
 }
-
-export default index;
+export default Users;
